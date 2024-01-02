a4f24b45e77c82989e35aa6d10e5de1299dcf870
Melvin0070 <melvin.aspirant@gmail.com>
2024-01-02 13:25:44 +0530

    Done

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Home from './component/Home';
-import Quiz from './component/Quiz';
-import Result from './component/Result';
+// import Quiz from './component/Quiz';
+// import Result from './component/Result';
 
 function App() {
   return (
